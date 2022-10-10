bff0c6e3dc7a14c5cfb32a8b253feff726664a8c
SDatar <SDatar@WBUR-MBP15-S-Datar-News.local>
2022-10-10 11:12:30 -0400

    Updating text and README

--- README.md
+++ README.md
@@ -1,10 +1,28 @@
-# Getting Started with Create React App
+# Colorado News Mapping Project
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project uses [Create React App](https://github.com/facebook/create-react-app). 
+
+## How it works
+It has been set up to pull in data from spreadsheets every 6 minutes using Github Actions. You will find it under `.github/workflows/data.yml`. The repository already has the necessary credentials for pulling in the necessary data from Google Sheets.
+
+There are two steps in the process:
+
+- `pullData.js`: This script pulls in the outlet data from the Google sheet. This contains information on news outlets identified by the project, ownership, circulation, coverage area and so on. It also pulls in the results from the news study that shows the originality and locality on the county level.
+- `processData.js`: This adds the study data to the map, so it can be displayed on the app.
+
+If you need to make any changes to the text above the map, it needs to be done manually.
+
+## Publishing
+The map is a static page with HTML/CSS and Javascript and is published to Github Pages on [this link](https://colabnewsco.github.io/news-map/). The task also publishes every 6 minutes, so you don't need to publish it manually.
+
+It is deployed as an iframe, the embed code for which is in `iframe.html`
 
 ## Available Scripts
 
 In the project directory, you can run:
+
+### `npm run get-sheet`
+Pulls in the data and processes it.
 
 ### `npm start`
 
@@ -14,11 +32,6 @@ Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 The page will reload when you make changes.\
 You may also see any lint errors in the console.
 
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
 ### `npm run build`
 
 Builds the app for production to the `build` folder.\
@@ -27,44 +40,6 @@ It correctly bundles React in production mode and optimizes the build for the be
 The build is minified and the filenames include the hashes.\
 Your app is ready to be deployed!
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+### `npm run deploy`
 
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+This publishes the new build to [this link](https://colabnewsco.github.io/news-map/).
--- src/App.js
+++ src/App.js
@@ -221,21 +221,18 @@ function App() {
         <Row className='justify-content-md-center'>
           <Col xs={12} sm={6}>
             <h1 className='App__hed bold'>Colorado News Mapping Project</h1>
-            <p className="App__desc">Where do Coloradans find their local news and community information, and what do we know about these sources? This map contains credentialed sources of local journalism — including newspapers, TV and radio stations, and digital news sites — and other sources that share or produce civic information — including community groups, organizational pages, or individuals.</p>
-            <p className="App__desc">This map is not complete, but is a work in progress. It was created by people from Colorado College, the University of Denver, Colorado Media Project, and the Colorado News Collaborative (COLab), Hearken, and others with support from the Online News Association.</p>
+            <p className="App__desc">Where do Coloradans find their local news and community information, and what do we know about these sources? This map contains credentialed sources of local journalism &mdash; including newspapers, TV and radio stations, and digital news sites &mdash; and other sources that share or produce civic information &mdash; including community groups, organizational pages, and individuals.</p>
+            <p className="App__desc">This map is a work in progress. It was created by people from Colorado College, the University of Denver, Colorado Media Project, Hearken, the Colorado News Collaborative (COLab), and others. This work is supported by a grant administered by the Online News Association with support from the Democracy Fund, Knight Foundation, the Inasmuch Foundation, and Scripps Howard Foundation.</p>
 
-            <p className="App__desc">Learn more about the map, methodology, partners, and funders <a href="https://colabnews.co/colorado-news-mapping-project" target='_parent'>here</a>.</p>
+            <p className="App__desc">Learn more about the map, methodology, partners, and funders <a href="https://colabnews.co/colorado-news-mapping-project-method/" target='_parent'>here</a>.</p>
             
             <ul>
-              <li><p className="App__desc"><strong>Hover over each county</strong> to see the number of local information sources identified to date, and the number of original, local news stories produced by some of the news sources in that county, in total,   on a single weekday in 2021.</p>
+              <li><p className="App__desc"><strong>Hover over each county</strong> to see the number of local information sources identified to date, and the number of original, local news stories produced by some of the news sources in that county that researchers counted on a single weekday in 2021. (Counties that don’t show a number of original and local stories hadn’t produced any for the day we researched.)</p>
               </li>
-              <li><p className="App__desc"><strong>Click or tap on a county</strong> to learn more about its news and information ecosystem and demographics, and to see a full list of the news sources we’ve identified to date along with media type, sector, ownership, language, and reach (if we know it).</p></li>
+              <li><p className="App__desc"><strong>Click or tap on a county</strong> to learn more about its news and information ecosystem and demographics, and to see a full list of the news and information sources we’ve identified so far along with media type, sector, ownership, language, and reach (if we know it).</p></li>
             </ul>
 
-            <p className="App_desc"><strong>Did we miss something? Did you find an error?</strong> Fill out <a href="https://docs.google.com/forms/d/e/1FAIpQLSdY3GCM61wsEVdEUpEJ6x6yErooyerovADATMby-IR7wdtkxQ/viewform" target='_parent'>this form</a> and let us know, so we can add or fix it. NOTE: News sources are placed on the map according to their physical address. Some counties might not show a source serving it because the publication’s address could be in a county next door. </p>
-
-            {/*<div className='map__subhed'><strong>Hover over each county</strong> to see the number of local information sources identified to date, and the number of original, local news stories produced by some of the news sources in that county, in total, on a single weekday in 2021.</div>
-            <div className='map__subhed'>Click or tap on a county to learn more about its news and information ecosystem and demographics, and to see a full list of the news sources we’ve identified to date along with media type, sector, ownership, language, and reach (if we know it).</div>*/}
+            <p className="App_desc"><strong>Did we miss something? Did you find an error?</strong> Fill out <a href="https://docs.google.com/forms/d/e/1FAIpQLSdY3GCM61wsEVdEUpEJ6x6yErooyerovADATMby-IR7wdtkxQ/viewform" target='_parent'>this form</a> and let us know so we can fix it. NOTE: News sources are placed on the map according to their physical office address, which means some might not appear in counties they also serve.</p>
           </Col>
           
           <Legend />
